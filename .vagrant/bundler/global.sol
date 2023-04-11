808c2df1dc1ffe140372cc99f95e6e53452e3892
nembo005 <nembotchedjou35@gmail.com>
2023-04-11 17:27:20 +0100

    cicd proceed

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"be27b3d56f79546ba9b702105faf6203d9a5d42096d9584219daa71db743cf51","vagrant_version":"2.3.4"}
